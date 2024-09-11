cbaa481f9b1fd85ed75867ab4d637f6fc61b2342
will4213 <wmcelwee@mail.pima.edu>
2024-09-11 11:27:53 -0700

    Update cis129_lab02_shortStory.py - added a short story.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+# Author: Will McElwee
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print("This is Bob.")
+print("He is a cool guy.")
+print("He likes soccer.")
